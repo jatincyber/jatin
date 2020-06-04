6e449e978019e6c1dd32ba2e68c3c8c95759ed4b
jatincyber <66360248+jatincyber@users.noreply.github.com>
2020-06-04 10:22:39 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,38 +1,26 @@
 <?php
-
-$name = $_REQUEST['name'];
-$email = $_REQUEST['email'];
-$number = $_REQUEST['number'];
-$subject = $_REQUEST['subject'];
-$message = $_REQUEST['message'];
-
-//check input
-
-if ((empty($name) ||  empty($email) ||  empty($number)  ||  empty($subject))  ||  empty($message)  )
-
-{
-    echo "Please fill all the feilds";
-
-}
-
-else
-
-{
-mail("alimentationequillibree@gmail.com ", "form", $message , "From: $name
-    <$email> <$number>  ") ;
-
-    echo "<script type='text/javascript'>alert('your message sent successfully') ;
-    window.history.log(-1);
-
-
-
-
-    </script>"           ;
-
-}
-
-
-
-
-
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'alimentationequillibree@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
 ?>
